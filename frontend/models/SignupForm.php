fd5dcb53bdfa78931eea4b936f35e1bbebadae49
IlMurlocDiplomato <cagni.mirco@icloud.com>
2022-08-27 22:02:51 +0200

    Integrate sb-admin 2 template in project

---
+++ frontend/models/SignupForm.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+use yii\base\Model;
+use common\models\User;
+
+/**
+ * Signup form
+ */
+class SignupForm extends Model
+{
+    public $firstname;
+    public $lastname;
+    public $username;
+    public $email;
+    public $password;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            //Rule for firstname
+            ['firstname', 'trim'],
+            ['firstname', 'required'],
+            ['firstname', 'string', 'min' => 2, 'max' => 15],
+            //Rule for surname
+            ['lastname', 'trim'],
+            ['lastname', 'required'],
+            ['lastname', 'string', 'min' => 2, 'max' => 15],
+
+//            ['username', 'trim'],
+//            ['username', 'required'],
+//            ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This username has already been taken.'],
+//            ['username', 'string', 'min' => 2, 'max' => 255],
+
+            ['email', 'trim'],
+            ['email', 'required'],
+            ['email', 'email'],
+            ['email', 'string', 'max' => 255],
+            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
+
+            ['password', 'required'],
+            ['password', 'string', 'min' => Yii::$app->params['user.passwordMinLength']],
+        ];
+    }
+
+    /**
+     * Signs user up.
+     *
+     * @return bool whether the creating new account was successful and email was sent
+     */
+    public function signup()
+    {
+        if (!$this->validate()) {
+            return null;
+        }
+
+        $user = new User();
+        $user->firstname = $this->firstname;
+        $user->lastname = $this->lastname;
+        //Set username as lastname.firstname
+        $user->username = $this->lastname . '.' . $this->firstname;
+        //$user->username = $this->username;
+        $user->email = $this->email;
+        $user->setPassword($this->password);
+        $user->generateAuthKey();
+        $user->generateEmailVerificationToken();
+
+        return $user->save() && $this->sendEmail($user);
+    }
+
+    /**
+     * Sends confirmation email to user
+     * @param User $user user model to with email should be send
+     * @return bool whether the email was sent
+     */
+    protected function sendEmail($user)
+    {
+        return Yii::$app
+            ->mailer
+            ->compose(
+                ['html' => 'emailVerify-html', 'text' => 'emailVerify-text'],
+                ['user' => $user]
+            )
+            ->setFrom([Yii::$app->params['supportEmail'] => Yii::$app->name . ' robot'])
+            ->setTo($this->email)
+            ->setSubject('Account registration at ' . Yii::$app->name)
+            ->send();
+    }
+}
